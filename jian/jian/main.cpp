6d34b64a54523fe5b0d059b842305993906b5c91
liuxiaoju <1032872992@qq.com>
2018-10-22 21:14:42 +0800

    打印矩阵

--- jian/jian/main.cpp
+++ jian/jian/main.cpp
@@ -27,42 +27,172 @@
 #include <stack>
 
 //2018/10/22 顺时针由外到内打印矩阵
+void printNumber(int number)
+{
+    printf("%d\t", number);
+}
+void PrintMatrixInCircle(int** numbers, int columns, int rows, int start)
+{
+    int endX = columns - 1 -start;
+    int endY = rows - 1 - start;
+    
+    //从左到右
+    for (int i = start; i <= endX ; ++ i) {
+        printNumber(numbers[start][i]);
+    }
+    if (start < endY) {
+    //从上到下
+    for (int i = start + 1; i <= endY; ++ i) {
+        printNumber(numbers[i][endX]);
+    }
+    }
+    
+    if (start  < endX && start< endY) {
+    //从右到左
+    for (int i = endX -1 ; i >= start; --i) {
+        printNumber(numbers[endY][i]);
+    }
+    }
+    if (start < endX && start < endY - 1) {
+    //从下到上
+    for (int i = endY -1; i >= start + 1; --i) {
+        printNumber(numbers[i][start]);
+    }
+    }
+}
+void PrintMatrixClockwisely(int** numbers,int columns,int  rows)
+{
+    if (numbers == NULL || columns <= 0 || rows <= 0)
+        return;
+    int start = 0;
+    while(columns >= start * 2 && rows >= start * 2)
+    {
+        PrintMatrixInCircle(numbers, columns, rows, start);
+        ++start;
+    }
+  
+}
+void Test(int columns, int rows)
+{
+    printf("Test Begin: %d columns, %d rows.\n", columns, rows);
+    
+    if(columns < 1 || rows < 1)
+        return;
+    
+    int** numbers = new int*[rows];
+    for(int i = 0; i < rows; ++i)
+    {
+        numbers[i] = new int[columns];
+        for(int j = 0; j < columns; ++j)
+        {
+            numbers[i][j] = i * columns + j + 1;
+        }
+    }
+    
+    PrintMatrixClockwisely(numbers, columns, rows);
+    printf("\n");
+    
+    for(int i = 0; i < rows; ++i)
+        delete[] (int*)numbers[i];
+    
+    delete[] numbers;
+}
+
 int main(int argc, char* argv[])
 {
     using namespace std;
+    /*
+     1
+     */
+    Test(1, 1);
+    
+    /*
+     1    2
+     3    4
+     */
+    Test(2, 2);
+    
+    /*
+     1    2    3    4
+     5    6    7    8
+     9    10   11   12
+     13   14   15   16
+     */
+    Test(4, 4);
+    
+    /*
+     1    2    3    4    5
+     6    7    8    9    10
+     11   12   13   14   15
+     16   17   18   19   20
+     21   22   23   24   25
+     */
+    Test(5, 5);
+    
+    /*
+     1
+     2
+     3
+     4
+     5
+     */
+    Test(1, 5);
+    
+    /*
+     1    2
+     3    4
+     5    6
+     7    8
+     9    10
+     */
+    Test(2, 5);
+    
+    /*
+     1    2    3
+     4    5    6
+     7    8    9
+     10   11   12
+     13   14   15
+     */
+    Test(3, 5);
+    
+    /*
+     1    2    3    4
+     5    6    7    8
+     9    10   11   12
+     13   14   15   16
+     17   18   19   20
+     */
+    Test(4, 5);
+    
+    /*
+     1    2    3    4    5
+     */
+    Test(5, 1);
+    
+    /*
+     1    2    3    4    5
+     6    7    8    9    10
+     */
+    Test(5, 2);
+    
+    /*
+     1    2    3    4    5
+     6    7    8    9    10
+     11   12   13   14    15
+     */
+    Test(5, 3);
+    
+    /*
+     1    2    3    4    5
+     6    7    8    9    10
+     11   12   13   14   15
+     16   17   18   19   20
+     */
+    Test(5, 4);
 
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
     return 0;
+    
 }
 
 
